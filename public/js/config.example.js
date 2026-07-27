bb6fe658103a7c112e707c35d34f3ba03bc04a0d
chrizDev <christian.14151123@gmail.com>
2026-07-27 16:46:16 -0600

    docs: actualizacion config.example.js

--- public/js/config.example.js
+++ public/js/config.example.js
@@ -1,9 +1,9 @@
  //Ejemplo de web app Firebase configuration
 const firebaseConfig = {
     apiKey: "TU_API_KEY",
-    authDomain: "TU_AUTH_DOMAIN",
-    projectId: "TU_PROYECT_ID",
-    storageBucket: "TU_STORAGE_BUCKET",
+    authDomain: "TU_PROYECTO.firebaseapp.com",
+    projectId: "TU_PROYECTO",
+    storageBucket: "TU_PROYECTO_.firebasestorage.app",
     messagingSenderId: "TU_MESSAGE_SENDER_ID",
     appId: "TU_APP_ID"
   };
